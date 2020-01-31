a5c4ebf4acb65a956394fa9c7d915cb44c939170
Khoi Hoang <hello@koicha.dev>
2020-01-31 21:32:11 +0100

    Updated menu-item to navigate withRoute HOC

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import { HomePage } from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div className="root">
-        <HomePage />
+    {/* 
+      Switch will only route on whatever path we are putting in our component. Nothing else! 
+      It will match first the path from the top and then below. It will not match up and down! 
+      Switch is useful, because it gives us more control of our code, because we don't accidently
+      Render multiple components
+    */}
+      <Switch>
+        <Route exact path='/' component={HomePage} /> 
+        <Route path='/hats' component={HatsPage} /> 
+      </Switch>
+
+
+
     </div>
   );
 }
--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    // BrowserRouter will give all the functionality of routing
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root'));
 
